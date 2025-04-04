d0acd86b04b623680579643b6a9611e5df0712b0
Viktor Ivanchenko <derfikus@gmail.com>
2025-04-04 12:53:59 +0300

    Create a structure

---
+++ Ubuntu-2004.sh
@@ -0,0 +1,28 @@
+# parameters
+
+
+# system update
+
+
+# configure docker repo
+
+
+# install packages
+
+
+# starting docker service
+
+
+# OPTIONAL install docker compose plugin
+
+
+# docker startup script
+
+
+# testing docker service
+
+
+# configure user privileges
+
+
+# END
