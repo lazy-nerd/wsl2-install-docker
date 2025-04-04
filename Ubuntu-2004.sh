dc5a39253268785ff33a7e97083734159b57ebee
Viktor Ivanchenko <derfikus@gmail.com>
2025-04-04 15:45:10 +0300

    Update system packages and install dependencies

--- Ubuntu-2004.sh
+++ Ubuntu-2004.sh
@@ -1,8 +1,18 @@
+#!/usr/bin/env bash
 # parameters
 
+localuser=$(id -u -n 1000)
 
 # system update
-
+printf "Updating system repositories... Please provide user password:\n"
+sudo apt-get update -qq >/dev/null
+sudo apt-get upgrade -y -qq >/dev/null
+echo "Installing prerequisite packages..."
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y -qq >/dev/null
 
 # configure docker repo
 
