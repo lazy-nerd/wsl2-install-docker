f6d0d6f2f20acbdce6c92b7ceeb767d80c8bc7c9
Viktor Ivanchenko <derfikus@gmail.com>
2025-04-09 14:30:30 +0300

    Fix spelling in printf

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -61,8 +61,8 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# install packages
-printf "Install docker packages...  "
+# Install packages
+printf "Installing docker packages...  "
 sudo apt-get update -qq >/dev/null 2>>err.log
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y -qq >/dev/null 2>>err.log
 is_ok
